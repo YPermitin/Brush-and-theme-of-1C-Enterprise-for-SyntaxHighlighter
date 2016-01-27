06d7cd6380a805ec49af425f02ed764c13b39217
YPermitin <ypermitin@yandex.ru>
2016-01-28 00:07:05 +0500

    Создание репозитория

---
+++ SyntaxHighlighterForOneCEnterprise/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("SyntaxHighlighterForOneCEnterprise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SyntaxHighlighterForOneCEnterprise")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("6728f73b-9242-45cf-9fa7-3b1e0f9ddc14")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SyntaxHighlighterForOneCEnterprise/Scripts/SyntaxHighlighter_OneCEnterprise/shBrushOce.js
@@ -0,0 +1,64 @@
+﻿
+/* version of 1C module for SyntaxHighliter + Composite C1 CMS */
+/* created by Permitin Y.A. aka YPermitin */
+/* http://www.develplatform.ru */
+
+; (function () {
+    // CommonJS
+    typeof (require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+
+    function Brush() {
+        // Формируем регулярное выражение для поиска ключевых слов
+        //var specChars = ' ()\=\-\+\\/\*.,;:\[\]\{\}';
+        var keywords = '';
+        var keyWordsList = ['Пока', 'Цикл', 'КонецЦикла', 'Для', 'По', 'Каждого',
+                            'Новый', 'Функция', 'Возврат', 'КонецФункции', 'Или', 'Перем', 'Экспорт', 'Процедура',
+                            'КонецПроцедуры', 'Если', 'Тогда', 'ИначеЕсли', 'КонецЕсли', 'Из', 'Не', 'Истина', 'Ложь',
+                            'Попытка', 'Исключение', 'КонецПопытки', 'Иначе', 'NULL', 'Неопределено', 'ВызватьИсключение',
+                            'While', 'For', 'Each',  'In', 'Do', 'To', 'New', 'Function', 'Return', 'EndDo', 
+                            'EndFunction', 'Or', 'Var', 'Export', 'Procedure', 'EndProcedure', 'If', 'Then', 
+                            'Else', 'ElsIf', 'EndIf', 'Not', 'True', 'False', 
+                            'Try', 'Except', 'Raise', 'EndTry', 'Undefined'
+                           ];
+        for (index = 0; index < keyWordsList.length; ++index) {
+            keywords = keywords + keyWordsList[index];
+            if (index == keyWordsList.length-1)
+                keywords = keywords;
+            else
+                keywords = keywords + '|';
+        }
+        keywords = '(^|\\s)(' + keywords + ')(?=\\s|$|;)';
+
+        function getKeywordsCyr(str) {
+            str = str
+                .replace(/^s+|s+$/g, '')
+                .replace(/s+/g, '|')
+            ;
+
+            var specChars = ' ()\=\-\+\\/\*.,;:\[\]\{\}';
+            return '(^|[^A-Za-z0-9а-яё])(' + str + ')(?=([' + specChars + ']|$))';
+        }
+
+
+        // Определяем регулярные выражения для каждого их вариантов подсветки
+        this.regexList = [
+            { regex: SyntaxHighlighter.regexLib.singleLineCComments, css: 'comments' },		        // Однострочный комментарий
+            { regex: /(["'])(?:(?!\1)[^\\]|\\.)*\1/gi, css: 'string' },		                        // Строка в двойных кавычках
+            { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },		        // Строка в одинарных кавычках
+            { regex: new RegExp('\\b([\\d]+(\\.[\\d]+)?|0x[a-f0-9]+)\\b', 'gi'), css: 'number' },	// Числа
+            { regex: /\+|\)|\(|\.|\\|\*|=|\:|;|\&lt;|\&gt;|\[|\]|\?/g, css: 'punctuation' },		// Пунктуация
+            //{ regex: getKeywordsCyr(keywords), css: 'keyword' },		                            // Ключевые слова
+            { regex: new RegExp(keywords, 'gi'), css: 'keyword' },		                            // Ключевые слова
+            { regex: /^\s*#.*/gm, css: 'preprocessor' },		                                    // Теги препроцессора вида #Область и #КонецОбласти
+            { regex: /^\s*&.*/gm, css: 'preprocessor' },		                                    // Теги препроцессора вида &Клиент and &Сервер
+        ];
+    }
+
+    Brush.prototype = new SyntaxHighlighter.Highlighter();
+    Brush.aliases = ['oce']; // Имя класса блока на странице, для которого будет использован этот скрипт
+
+    SyntaxHighlighter.brushes.Bash = Brush;
+
+    // CommonJS
+    typeof (exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
