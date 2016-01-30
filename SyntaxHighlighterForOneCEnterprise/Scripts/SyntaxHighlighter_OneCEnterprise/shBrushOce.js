cf7f4a8e89e4e822020d227b46afa5b1097e427c
YPermitin <ypermitin@yandex.ru>
2016-01-30 19:45:28 +0500

    1. Исправлены регулярные выражения для пунктураии (не находилась запятая) 2. для ключевых слов (не определялось слово знач и val в определении функций и процедуры) 3.  исправлено определение строк (если в конце был слэш).

--- SyntaxHighlighterForOneCEnterprise/Scripts/SyntaxHighlighter_OneCEnterprise/shBrushOce.js
+++ SyntaxHighlighterForOneCEnterprise/Scripts/SyntaxHighlighter_OneCEnterprise/shBrushOce.js
@@ -8,16 +8,15 @@
     typeof (require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
     function Brush() {
-        // Формируем регулярное выражение для поиска ключевых слов
-        //var specChars = ' ()\=\-\+\\/\*.,;:\[\]\{\}';
+        // Формируем регулярное выражение для поиска ключевых слов        
         var keywords = '';
-        var keyWordsList = ['Пока', 'Цикл', 'КонецЦикла', 'Для', 'По', 'Каждого',
+        var keyWordsList = ['Пока', 'Цикл', 'КонецЦикла', 'Для', 'По', 'Каждого', 'Знач',
                             'Новый', 'Функция', 'Возврат', 'КонецФункции', 'Или', 'Перем', 'Экспорт', 'Процедура',
                             'КонецПроцедуры', 'Если', 'Тогда', 'ИначеЕсли', 'КонецЕсли', 'Из', 'Не', 'Истина', 'Ложь',
                             'Попытка', 'Исключение', 'КонецПопытки', 'Иначе', 'NULL', 'Неопределено', 'ВызватьИсключение',
                             'While', 'For', 'Each',  'In', 'Do', 'To', 'New', 'Function', 'Return', 'EndDo', 
                             'EndFunction', 'Or', 'Var', 'Export', 'Procedure', 'EndProcedure', 'If', 'Then', 
-                            'Else', 'ElsIf', 'EndIf', 'Not', 'True', 'False', 
+                            'Else', 'ElsIf', 'EndIf', 'Not', 'True', 'False', 'Val',
                             'Try', 'Except', 'Raise', 'EndTry', 'Undefined'
                            ];
         for (index = 0; index < keyWordsList.length; ++index) {
@@ -27,27 +26,14 @@
             else
                 keywords = keywords + '|';
         }
-        keywords = '(^|\\s)(' + keywords + ')(?=\\s|$|;)';
+        keywords = '(^|\\s|[;()])(' + keywords + ')(?=\\s|$|[;()])';
 
-        function getKeywordsCyr(str) {
-            str = str
-                .replace(/^s+|s+$/g, '')
-                .replace(/s+/g, '|')
-            ;
-
-            var specChars = ' ()\=\-\+\\/\*.,;:\[\]\{\}';
-            return '(^|[^A-Za-z0-9а-яё])(' + str + ')(?=([' + specChars + ']|$))';
-        }
-
-
-        // Определяем регулярные выражения для каждого их вариантов подсветки
         this.regexList = [
             { regex: SyntaxHighlighter.regexLib.singleLineCComments, css: 'comments' },		        // Однострочный комментарий
-            { regex: /(["'])(?:(?!\1)[^\\]|\\.)*\1/gi, css: 'string' },		                        // Строка в двойных кавычках
-            { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },		        // Строка в одинарных кавычках
-            { regex: new RegExp('\\b([\\d]+(\\.[\\d]+)?|0x[a-f0-9]+)\\b', 'gi'), css: 'number' },	// Числа
-            { regex: /\+|\)|\(|\.|\\|\*|=|\:|;|\&lt;|\&gt;|\[|\]|\?/g, css: 'punctuation' },		// Пунктуация
-            //{ regex: getKeywordsCyr(keywords), css: 'keyword' },		                            // Ключевые слова
+            { regex: /(["'])(?:(?!\1)[^\\]|\\|\\.)*\1/gi, css: 'string' },		                        // Строка в двойных кавычках
+            { regex: new RegExp('\'(?:\\?.)*?\'', 'gi'), css: 'string' },		                    // Строка в одинарных кавычках
+            { regex: new RegExp('(^|\\s)([\\d]+(\\.[\\d]+)?|0x[a-f0-9]+)(?=\\s|$|;)', 'gi'), css: 'number' },	// Числа
+            { regex: /\+|\)|\(|\.|\,|\\|\*|=|\:|;|\&lt;|\&gt;|\[|\]|\?/g, css: 'punctuation' },		// Пунктуация
             { regex: new RegExp(keywords, 'gi'), css: 'keyword' },		                            // Ключевые слова
             { regex: /^\s*#.*/gm, css: 'preprocessor' },		                                    // Теги препроцессора вида #Область и #КонецОбласти
             { regex: /^\s*&.*/gm, css: 'preprocessor' },		                                    // Теги препроцессора вида &Клиент and &Сервер
